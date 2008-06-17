30e86857a559572e094882fe55b3f59669dfc6a5
lars <lars@6ae05860-910a-4080-baf6-befe656c970c>
2008-06-17 13:41:40 +0000

    Adding -dev appendix
    git-svn-id: https://svn.usrportage.de/php-mogilefs@58 6ae05860-910a-4080-baf6-befe656c970c

--- trunk/php_mogilefs.h
+++ trunk/php_mogilefs.h
@@ -18,7 +18,7 @@
 
 #ifndef PHP_MOGILEFS_H
 #define PHP_MOGILEFS_H
-#define PHP_MOGILEFS_VERSION "0.7.4"
+#define PHP_MOGILEFS_VERSION "0.7.5-dev"
 
 extern zend_module_entry mogilefs_module_entry;
 #define phpext_mogilefs_ptr &mogilefs_module_entry
