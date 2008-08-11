05d03400085ab7279a77afbc5e22a51ca7e44a13
lars <lars@6ae05860-910a-4080-baf6-befe656c970c>
2008-08-11 18:04:12 +0000

    Adding redesign blueprint
    git-svn-id: https://svn.usrportage.de/php-mogilefs@80 6ae05860-910a-4080-baf6-befe656c970c

---
+++ branches/api-redesign/blueprint/classes.php
@@ -0,0 +1,53 @@
+<?php
+class MogileFsDomain
+{
+	public function __construct($domain, $host, $port = 6001);
+	public function getName();
+	public function getHost();
+	public function getPort();
+}
+
+class MogileFsFile
+{
+	public function __construct($name, $class = null);
+	public function getName();
+	public function getDomain();
+	public function getContent();
+	public function setContent($string);
+}
+
+class MogileFsClass
+{
+	public function __construct($name, $file_count = 1);
+	public function setFileCount($count);
+	public function getFileCount();
+	public function getName();
+}
+
+class MogileFsTracker
+{
+	public function connect($host, $port, $timeout = 30);
+	public function close();
+
+	public function createDomain(MogileFsDomain $domain); // bool
+	public function removeDomain(MogileFsDomain $domain); // bool
+	public function getDomain($name); // MogileFsDomain
+	public function getDomains();  // MogileFsDomain[]
+
+	public function createClass(MogileFsClass $class); // bool
+	public function modifyClass(MogileFsClass $class); // bool
+	public function getClass($name); // MogileFsClass
+	public function getClasses(); // MogileFsClass[]
+}
+
+class MogileFs
+{
+	public function __construct(
+		MogileFsTracker $tracker,
+		MogileFsDomain $domain,
+		MogileFsClass $class
+	);
+	public function put(MogileFsFile $file); // bool
+	public function get($name); // MogileFsFile
+	public function drop($name); // bool
+}
