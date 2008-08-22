1b9ab78a6a7b36c2188e41006f0db785f31d7a0f
lars <lars@6ae05860-910a-4080-baf6-befe656c970c>
2008-08-22 14:14:40 +0000

    Towards a new release
    git-svn-id: https://svn.usrportage.de/php-mogilefs/trunk@106 6ae05860-910a-4080-baf6-befe656c970c

--- php_mogilefs.h
+++ php_mogilefs.h
@@ -32,7 +32,7 @@
 
 #ifndef PHP_MOGILEFS_H
 #define PHP_MOGILEFS_H
-#define PHP_MOGILEFS_VERSION "0.7.5b1"
+#define PHP_MOGILEFS_VERSION "0.7.5b3"
 
 extern zend_module_entry mogilefs_module_entry;
 #define phpext_mogilefs_ptr &mogilefs_module_entry
